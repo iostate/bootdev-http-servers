c3cbea5ed47b2bdcbae6260fe89169274055b95e
Quan Truong <qmtruong92@gmail.com>
2025-02-25 20:01:50 -0700

    clean up validate chirp handler

--- validate_chirp_handler.go
+++ validate_chirp_handler.go
@@ -21,47 +21,26 @@ func ValidateChirpHandler(w http.ResponseWriter, r *http.Request) {
 		Valid bool `json:"valid"`
 	}
 
-	type cleanedBody struct {
+	type cleanedResponse struct {
 		CleanedBody string `json:"cleaned_body"`
 	}
 
 	var reqBody requestBody
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&reqBody)
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
 	if err != nil {
-		log.Printf("error decoding json: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		resp, _ := json.Marshal(errorResponse{Error: "Something went wrong"})
-		w.Write(resp)
+		log.Printf("Error decoding json: %v", err)
+		respondWithError(w, http.StatusInternalServerError, "Error decoding json: ", err)
 		return
 	}
 
 	if len(reqBody.Body) > 140 {
-		w.WriteHeader(http.StatusBadRequest)
-		resp, err := json.Marshal(errorResponse{Error: "Chirp is too long"})
-		if err != nil {
-			log.Printf("Error marshalling error response: %v", err)
-			w.Write([]byte(`"error":"Something went wrong`))
-			return
-		}
-		w.Write(resp)
+		respondWithError(w, http.StatusBadRequest, "Chirp is longer than 140 characters", err)
 		return
 	} 
 
 	// Sanitize bad words
 	cleanedChirp := replaceBadWords(reqBody.Body)
-
-	// Chirp is valid
-	// resp, err := json.Marshal(validResponse{Valid: true})
-	resp, err := json.Marshal(cleanedBody{CleanedBody: cleanedChirp})
-	if err != nil {
-		log.Printf("Error marshalling response: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`"error":"Something went wrong`))
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+	respondWithJSON(w, http.StatusOK, cleanedResponse{CleanedBody: cleanedChirp})
 }
 
 // Replaces any bad words, words are preset in the function
